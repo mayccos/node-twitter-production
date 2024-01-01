de04da18f65d64af8151bfc2827bfac4688367df
mayccos <mothos@hotmail.fr>
2024-01-02 00:45:04 +0100

    fix

--- environment/development.js
+++ environment/development.js
@@ -6,4 +6,6 @@ module.exports = {
     dbUrl: `mongodb+srv://mayccos:${process.env.MONGODB_PASSWORD}@cluster-projet-14.nxgcf.mongodb.net/twitter`,
     cert: path.join(__dirname, '../ssl/local.crt'),
     key: path.join(__dirname, '../ssl/local.key'),
+    portHttp: 3000,
+    portHttps: 3001,
 }
--- environment/production.js
+++ environment/production.js
@@ -4,4 +4,6 @@ module.exports = {
     dbUrl: `mongodb+srv://mayccos:${process.env.MONGODB_PASSWORD}@cluster-projet-14.nxgcf.mongodb.net/twitter`,
     cert: '/etc/letsencrypt/live/www.mayccos.fr/fullchain.pem',
     key: '/etc/letsencrypt/live/www.mayccos.fr/privkey.pem',
+    portHttp: 80,
+    portHttps: 443,
 }
